e50e70fe1ae462a36205a81c06b1a373e51fb906
Gustavo Bays <dooca@192.168.1.11>
2023-04-20 19:59:51 -0300

    feat: added token comlumn on users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('token', 512);
             $table->timestamps();
         });
     }
