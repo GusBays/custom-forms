8a3e4ba069c2a2ba9d3fdd63bcd57b35d46d309b
Gustavo Bays <dooca@192.168.1.11>
2023-04-20 20:53:38 -0300

    feat: added methods to get database registers with filters

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseRepository
@@ -27,5 +28,38 @@ class BaseRepository
     public function getModelRules(): array
     {
         return $this->model->getRules();
+    }
+
+    protected function getOneByOrFail(string $field, $value): Model
+    {
+        $this->whereBy(...func_get_args());
+
+        try {
+            $model = $this->query->firstOrFail();
+        } catch (\Throwable $th) {
+            throw $th;
+        } finally {
+            $this->getNewQuery();
+        }
+
+        return $model;
+    }
+
+    protected function getNewQuery(): void
+    {
+        $this->model = app($this->model->getMorphClass());
+        $this->query = $this->model->query();
+
+        $this->bootQuery();
+    }
+
+    protected function bootQuery(): void
+    {
+        $this->query->addSelect($this->model->getTable() . '.*');
+    }
+
+    protected function whereBy(string $field, $value = null)
+    {
+        $this->query->where($field, $value);
     }
 }
