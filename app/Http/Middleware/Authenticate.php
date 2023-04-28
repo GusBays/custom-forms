7f2d02e13bafca282e9881c1d66c202dda45433d
Gustavo Bays <dooca@192.168.1.4>
2023-04-27 21:53:50 -0300

    feat: define organization id on global config to use query global scopes

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,13 +5,33 @@ namespace App\Http\Middleware;
 use App\Contracts\CookieEnum;
 use App\Contracts\RedirectEnum;
 use App\Helpers\Routes;
+use App\Models\User;
+use Closure;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
+    private User $user;
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (!$request->bearerToken()) throw new AuthenticationException();
+
+        $auth = $this->auth->guard(...$guards);
+
+        if (!$auth->check()) throw new AuthorizationException('authorization_failed', Response::HTTP_UNAUTHORIZED);
+        
+        $this->user = $auth->user();
+
+        $this->setGlobalConfigs();
+
+        return $next($request);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -21,5 +41,12 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         //
+    }
+
+    private function setGlobalConfigs(): void
+    {
+        config(['organization_id' => $this->user->organization_id]);
+        config(['user_id' => $this->user->id]);
+        config(['user_type' => $this->user->type]);
     }
 }
