7161dfb1511bbc8b323398328726b1f54db8906a
Gustavo Bays <dooca@192.168.1.4>
2023-06-02 21:16:35 -0300

    feat: created functions to notify filler and send pdf file with form filled

--- app/Jobs/AnswerNotificationJob.php
+++ app/Jobs/AnswerNotificationJob.php
@@ -5,12 +5,18 @@ namespace App\Jobs;
 use App\Datas\Filler\FillerUpdateData;
 use App\Datas\Form\FormUpdateData;
 use App\Filters\Filler\FillerIdFilter;
+use App\Filters\FormFieldAnswer\FormFieldAnswerFormIdFillerIdFilter;
+use App\Notifications\Front\AnswerNotification;
 use App\Repositories\FillerRepository;
+use App\Repositories\FormFieldAnswerRepository;
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
 
 class AnswerNotificationJob implements ShouldQueue
 {
@@ -18,6 +24,8 @@ class AnswerNotificationJob implements ShouldQueue
 
     private FormUpdateData $form;
     private FillerUpdateData $filler;
+    private FillerRepository $fillerRepository;
+    private FormFieldAnswerRepository $formFieldAnswerRepository;
 
     public function __construct(
         FormUpdateData $form,
@@ -26,20 +34,55 @@ class AnswerNotificationJob implements ShouldQueue
     {
         $this->form = $form;
         $this->filler = $filler;
+        $this->fillerRepository = app(FillerRepository::class);
+        $this->formFieldAnswerRepository = app(FormFieldAnswerRepository::class);
     }
-    
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
-    public function handle()
+
+    public function handle(): void
     {
-        /** @var FillerRepository */
-        $fillerRepository = app(FillerRepository::class);
+        config(['organization_id' => $this->form->getOrganizationId()]);
 
-        $filler = $fillerRepository->getNotifiableInstance(new FillerIdFilter($this->filler->getId()));
+        $filler = $this->fillerRepository->getNotifiableInstance(new FillerIdFilter($this->filler->getId()));
 
-        $filler->notify('mail');
+        $file = $this->putFileToStorageAndGetPath();
+
+        $filler->notify(new AnswerNotification($this->filler, $this->form, storage_path('app/'.$file)));
+    }
+
+    private function putFileToStorageAndGetPath(): string
+    {
+        $organizationId = $this->form->getOrganizationId();
+        $formId = $this->form->getId();
+        $fileName = 'fomulario-' . $formId . '-preenchido-por-' . $this->filler->getName() . '.pdf';
+
+        $path  = "public/$organizationId/forms/$formId/$fileName";
+
+        $pdf = $this->getPdfInstance();
+        $pdf->loadView('pdf.form-filled', $this->getData());
+
+        Storage::put($path, $pdf->download("$fileName")->getOriginalContent());
+
+        return $path;
+    }
+
+    private function getData(): array
+    {
+        $answers = $this->formFieldAnswerRepository->getAnswersBy(
+            new FormFieldAnswerFormIdFillerIdFilter(
+                $this->form->getId(),
+                $this->filler->getId()
+            )
+        );
+
+        return [
+            'filler' => $this->filler,
+            'form' => $this->form,
+            'answers' => $answers
+        ];
+    }
+
+    private function getPdfInstance(): PDF
+    {
+        return app(PDF::class);
     }
 }
