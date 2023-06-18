324c4d0414f28679e278d571669a80647d8a0b35
Bagy <bagy@MacBook-Air-de-Bagy.local>
2023-06-18 20:15:03 -0300

    feat: added modal confirmation for logoff

--- resources/views/snippets/sidebar.blade.php
+++ resources/views/snippets/sidebar.blade.php
@@ -39,12 +39,34 @@
                     </ul>
                 </div>
                 <div class="modal-footer">
-                    <a type="button" href="/action/logoff" class="btn btn-danger">Sair</a>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="background-color:#7800D2;border:none;">Fechar</button>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#logoff">
+                        Sair
+                    </button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Fechar
+                    </button>
                 </div>
             </div>
         </div>
     </div>
+
+  <div class="modal fade" id="logoff" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Deseja realmente sair?</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Não esqueça de salvar todos os dados antes de fazer logoff.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+          <a type="button" class="btn btn-danger" href="/action/logoff">Sair</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
     </div>
