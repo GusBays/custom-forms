c1f54ad28f33d497d7a789b2ba9bc3f54cb6655f
Bagy <bagy@MacBook-Air-de-Bagy.local>
2023-06-18 18:45:53 -0300

    feat: new sidebar view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ env('APP_URL') }}/assets/notebook-icon.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>
         @yield('title')
     </title>
 </head>
 <body class="bg-light">
     @yield('content')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,29 +1,50 @@
-<nav class="container navbar-nav navbar-fixed-top">
-    <ul>
-        <li class="row">
-            <a href="/admin">
-                <h3>Início</h3>
-            </a>
-        </li>
-        <li class="row">
-            <a href="/formularios">
-                <h3 class="">Formulários</h3>
-            </a>
-        </li>
-        <li class="row">
-            <a href="/usuarios">
-                <h3>Usuários</h3>
-            </a>
-        </li>
-        <li class="row">
-            <a href="/relatorios">
-               <h3>Relatórios</h3>
-            </a>
-        </li>
-        <li class="row">
-            <a href="/preenchedores">
-                <h3>Preenchedores</h3>
-            </a>
-        </li>
-    </ul>
+<nav class="navbar mb-3">
+    <div class="container">
+
+    <a href="/admin" class="navbar-brand">
+        <img src="{{ env('APP_URL') }}/assets/notebook-icon.svg" alt="notebook icon" width="30" height="24" class="d-inline-block align-text-top">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="modal" data-bs-target="#sidebar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="modal fade" id="sidebar" tabindex="-1" aria-hidden="true" style="width: 300px">
+        <div class="modal-dialog modal-fullscreen">
+            <div class="modal-content" style="width:300px">
+                <div class="modal-header">
+                    <img src="{{ env('APP_URL') }}/assets/notebook-icon.svg" alt="notebook icon" width="30" height="24" class="d-inline-block align-text-top">
+                <button type="button" class="btn-close text-start" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/admin">Início</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/formularios">Formulários</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/formulários/respostas">Respostas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/preenchedores">Preenchedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/relatorios">Relatórios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="/usuarios">Usuários</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="background-color:#7800D2;border:none;">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
 </nav>
