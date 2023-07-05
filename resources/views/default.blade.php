42785624d15cd33f1e71e8ae8f95c473dafc7c87
Bagy <bagy@192.168.1.9>
2023-07-05 20:25:18 -0300

    fix: icon url

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="{{ env('APP_URL') }}/assets/notebook-icon.svg">
+    <link rel="icon" type="image/x-icon" href="{{ env('APP_URL') }}/assets/img/notebook-icon.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ env('APP_URL') }}/assets/css/app.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
