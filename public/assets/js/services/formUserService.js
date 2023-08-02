718515b59dbf6bb167a4aa37181cea297035ba82
Gustavo Bays <bagy@192.168.1.3>
2023-08-02 20:29:18 -0300

    feat: added functions to add new users to form

--- public/assets/js/services/formUserService.js
+++ public/assets/js/services/formUserService.js
@@ -5,8 +5,14 @@ const api = new Api('form-users')
 
 const formUsersIdInput = document.querySelectorAll('#form-user-id')
 const formUsersTypeSelect = document.querySelectorAll('#form-user-type')
+const formUsersEmail = document.querySelectorAll('#form-user-email')
 
 const deleteButtons = document.querySelectorAll('#delete-user')
+const addUserModalButton = document.getElementById('add-user-modal-button')
+const addUserCreateButton = document.getElementById('create-form-users')
+
+if (addUserModalButton) registerAddUserModalButtonEventListener()
+if (addUserCreateButton) registerCreateUserButtonEventListener()
 
 export function getFormUsers() {
     const formUsers = []
@@ -40,4 +46,99 @@ export function getFormUsers() {
     }
 
     return formUsers
+}
+
+function registerAddUserModalButtonEventListener() {
+    addUserModalButton.addEventListener('click', () => {
+        const userApi = new Api('users')
+        userApi.get()
+        .then(res => {
+            if (!res.ok) throw Error(res.statusText)
+            else return res.json()
+        })
+        .then(response => {
+            loadTable(response.data)
+        })
+        .catch(e => {
+            return new Toast()
+                .show('Ocorreu um erro ao buscar os usuários cadastrados, tente novamente.')
+        })
+    })
+}
+
+const formUsersToCreate = []
+
+function loadTable(data) {
+    const tableBody = $('#users-list')
+    tableBody.empty()
+
+    var html = ''
+
+    for (let user of data) {
+
+        if (Array.from(formUsersEmail).find(element => element.innerText === user.email)) continue
+
+        html += `
+            <tr class="col">
+                <td><input type="checkbox" value="${user.id}" id="create-user-checkbox"></td>
+                <td>${user.email}</td>
+                <td>
+                    <select class="form-select" id="create-user-type" data-user-id=${user.id}>
+                        <option value="editor">Editor</option>
+                        <option value="viewer">Visualizador</option>
+                    </select>
+                </td>
+            </tr>
+        `
+    }
+
+    if ('' === html) html += '<div class="mt-3">Nenhum usuário encontrado</div>'
+
+    tableBody.append(html)
+
+    const formId = document.getElementById('id').value
+    const createUserCheckbox = document.querySelectorAll('#create-user-checkbox')
+    const createFormUserTypeSelect = document.querySelectorAll('#create-user-type')
+    
+    for (let checkbox of createUserCheckbox) {
+        const newFormUserObj = {
+            form_id: formId,
+            user_id: checkbox.value
+        }
+
+        formUsersToCreate.push(newFormUserObj)
+
+        const index = formUsersToCreate.indexOf(newFormUserObj)
+
+        checkbox.addEventListener('change', () => {
+            if (!checkbox.checked) formUsersToCreate.splice(index, 1)
+
+            let selectFormUserType = Array.from(createFormUserTypeSelect).find(element => element.dataset.userId === checkbox.value)
+            formUsersToCreate[index].type = selectFormUserType.options[selectFormUserType.selectedIndex].value
+            selectFormUserType.addEventListener('change', () => {
+                formUsersToCreate[index].type = selectFormUserType.options[selectFormUserType.selectedIndex].value
+            })
+
+        })
+    }
+}
+
+function registerCreateUserButtonEventListener() {
+    addUserCreateButton.addEventListener('click', () => {
+        formUsersToCreate.forEach(formUser => {
+
+            if (!formUser.type) return;
+
+            api.post(formUser)
+            .then(res => {
+                if (!res.ok) throw Error(res.statusText)
+            })
+            .catch(e => {
+                return new Toast()
+                    .show('Ocorreu um erro ao adicionar usuario ao formulário, tente novamente.')
+            })
+        })
+
+        window.location.reload()
+    })
 }
--- resources/views/sidebar/forms/form-detail.blade.php
+++ resources/views/sidebar/forms/form-detail.blade.php
@@ -91,7 +91,7 @@
                                 <tr class="col">
                                     <input type="text" id="form-user-id" value="{{ $formUser->getId() }}" hidden>
                                     <td>{{ $formUser->getUser()->getName() }}</td>
-                                    <td>{{ $formUser->getUser()->getEmail() }}</td>
+                                    <td id="form-user-email">{{ $formUser->getUser()->getEmail() }}</td>
                                     <td>
                                         <select id="form-user-type" class="form-select" data-form-user-id="{{ $formUser->getId() }}">
                                             <option 
@@ -113,11 +113,45 @@
                                                 value="viewer">Visualizador</option>
                                         </select>
                                     </td>
-                                    <td><button class="btn btn-danger" id="delete-user" data-form-user-id="{{ $formUser->getId() }}">Remover usuário</button></td>
+                                    <td><button class="btn btn-danger" id="delete-user" data-form-user-id="{{ $formUser->getId() }}"
+                                        @if ('creator' === $formUser->getType())
+                                            disabled
+                                        @endif
+                                        >Remover usuário</button></td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
+                    <button  type="button" class="btn btn-success" id="add-user-modal-button" data-bs-toggle="modal" data-bs-target="#addUserModal">Adicionar novo usuário ao formulário</button>
+                </div>
+
+                <div class="modal fade" id="addUserModal" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Adicionar usuário ao formulário</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Adicionar</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Tipo</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="users-list">
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary border-0" data-bs-dismiss="modal">Voltar</button>
+                                <button type="button" class="btn btn-success border-0 theme-color" id="create-form-users">Adicionar usuários</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div id="fields" data-tab-info>
