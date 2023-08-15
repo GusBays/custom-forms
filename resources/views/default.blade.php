c8dc968181273e02b80b6887e4f2ea532a49ea77
Gustavo Bays <bagy@192.168.1.3>
2023-08-14 21:48:45 -0300

    feat: added new ajax scripts on default view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -10,6 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
     <script src="{{ env('APP_URL') }}/assets/js/buttons/back.js"></script>
     @yield('scripts')
     <title>
@@ -19,5 +20,11 @@
 <body class="bg-light">
     @include('snippets/toast')
     @yield('content')
+
+    <!-- account scripts -->
+    @include('scripts/admin/account/login')
+    @include('scripts/admin/account/logoff')
+    @include('scripts/admin/account/recover')
+    @include('scripts/admin/account/register')
 </body>
 </html>
